149837e10f61b957b99b4252073fc3defeef1620
Vladislav Krivokon <vladeslimonade@gmail.com>
2018-10-24 22:08:10 +0300

    Updates

---
+++ precache-manifest.e8e42ff7584741c22b6fe09b5c4887d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "2dccc99b90bb3d84ef08",
+    "url": "/weather-app/static/js/main.2dccc99b.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/weather-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "2dccc99b90bb3d84ef08",
+    "url": "/weather-app/static/css/main.9f476caa.chunk.css"
+  },
+  {
+    "revision": "170f7bb9bbaf95e06ae03c8ee00dfe2d",
+    "url": "/weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8455aa691de967c14a46123e2e30644d.js"
+  "/weather-app/precache-manifest.e8e42ff7584741c22b6fe09b5c4887d8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/weather-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
