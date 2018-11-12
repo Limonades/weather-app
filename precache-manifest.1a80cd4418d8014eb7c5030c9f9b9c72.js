dd6c7f96db1536eebbe150603097e57e7d03acb8
Vladislav Krivokon <vladeslimonade@gmail.com>
2018-11-12 16:57:00 +0200

    Updates

---
+++ precache-manifest.1a80cd4418d8014eb7c5030c9f9b9c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0315fe014ea1115abf8bf0f1fe4cc6fa",
+    "url": "/weather-app/static/media/loader.0315fe01.gif"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "6122f1cb37ab2e5d2efa",
+    "url": "/weather-app/static/js/main.6122f1cb.chunk.js"
+  },
+  {
+    "revision": "526ebfefaf01d5647900",
+    "url": "/weather-app/static/js/1.526ebfef.chunk.js"
+  },
+  {
+    "revision": "6122f1cb37ab2e5d2efa",
+    "url": "/weather-app/static/css/main.3c72c567.chunk.css"
+  },
+  {
+    "revision": "3c5409f1e5ef69d0778551610a90ff69",
+    "url": "/weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.c70a7c7f5b170214187a5f69d5d86519.js"
+  "/weather-app/precache-manifest.1a80cd4418d8014eb7c5030c9f9b9c72.js"
 );
 
 workbox.clientsClaim();
