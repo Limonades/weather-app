c14e3da728430a6af20b1e28e2d9e11f0006bda9
Vladislav Krivokon <vladeslimonade@gmail.com>
2018-11-13 18:48:42 +0200

    deleted css and html comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App'
+import './App.sass';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
